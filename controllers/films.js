d49c4d6e0f28becbbf95f7c67206502ce341a84b
Fraser McKay <frasermckay97@gmail.com>
2018-06-18 16:53:33 +0100

    finished films.js

--- controllers/films.js
+++ controllers/films.js
@@ -1,3 +1,35 @@
 const films = require('../models/films')();
 const Film = require('../models/film');
 const Review = require('../models/review');
+const router = new express.Router();
+
+router.get('/:id', function(req, res){
+  const index = req.params.id;
+  res.json({films: films[index]})
+})
+
+router.delete('/:id', function(req, res){
+const index = req.params.id;
+films.splice(index, 1);
+res.json({films: films});
+})
+
+router.put('/:id', function(req, res){
+const index = req.params.id;
+films[index] = new Film(req.body);
+res.json({films: films});
+})
+
+router.get('/', function(req, res){
+  res.json({films: films});
+})
+
+router.post('/', function(req, res){
+  const newFilm = new Film(
+    req.body
+  );
+  films.push(newFilm);
+  res.json({films: films});
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,15 @@
 const express = require('express');
 const router = new express.Router();
 
+router.use('/film', require('./film.js'));
+
+
+router.get('/', function(req, res) {
+  res.json({ data: 'Welcome!'});
+});
+
+router.get('/about', function(req, res){
+  res.json({data: "All about us!"})
+});
+
 module.exports = router;
